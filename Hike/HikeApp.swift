2813cdf8462a10798db293bcfaa0618e99567664
Jessi Ju <sbjoo@pntbiz.com>
2023-07-21 19:29:16 +0900

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by PNT001 on 2023/07/21.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
